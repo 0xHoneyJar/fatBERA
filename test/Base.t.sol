a6aab27487a188cab5b95f406047f2401cbe52d5
exp.table <exp.table@protonmail.com>
2025-01-21 10:39:28 +0100

    using upgrades

--- test/Base.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-import {Test} from "forge-std/Test.sol";
-import {console2} from "forge-std/console2.sol";
-
-contract BaseTest is Test {
-    function setUp() public virtual {}
-}
