fae83764767db7d151344444691e11ca0115b846
exp.table <exp.table@protonmail.com>
2025-05-06 22:53:36 +0200

    updated stfatbera deploy script

--- script/DeployStakedFatBera.s.sol
+++ script/DeployStakedFatBera.s.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.23;
 
 import "forge-std/Script.sol";
+import { fatBERA as FatBERA } from "../src/fatBERA.sol";
 import "../src/StakedFatBERA.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract Deploy is Script {
     address constant FATBERA    = 0xBAE11292A3E693aF73651BDa350D752AE4A391D4;
     address constant OPERATOR   = 0xaF582c3335D51F2DFd749F9a476eBEAb6eC5233D;
+    address constant MS         = 0x0000000000000000000000000000000000000000;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -20,6 +22,17 @@ contract Deploy is Script {
         StakedFatBERA stFatBera = StakedFatBERA(proxy);
 
         stFatBera.grantRole(stFatBera.OPERATOR_ROLE(), OPERATOR);
+        stFatBera.grantRole(stFatBera.ADMIN_ROLE(), MS);
+
+
+        // mint shares that will be locked forever
+        FatBERA(FATBERA).depositNative{value: 1 ether}(deployer);
+        FatBERA(FATBERA).approve(address(stFatBera), 1 ether);
+        stFatBera.deposit(1 ether, deployer);
+        stFatBera.transfer(address(0x42069), stFatBera.balanceOf(deployer));
+
+        // remove admin role from deployer
+        stFatBera.revokeRole(stFatBera.ADMIN_ROLE(), deployer);
 
         vm.stopBroadcast();
     }
